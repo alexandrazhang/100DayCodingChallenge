273b76d79aafa033b001c6f579c7b7f69a4ff886
yanlizhang <alexandrazhang0626@gmail.com>
2018-05-09 22:05:51 -0700

    day 4 start

--- FromDay4/Day4LC665.java
+++ FromDay4/Day4LC665.java
@@ -1,4 +1,24 @@
 package CodingChallengeEasy.FromDay4;
 
 public class Day4LC665 {
+    public boolean checkPossibility(int[] nums) {
+        if(nums == null || nums.length <= 2) {
+            return true;
+        }
+        int count = 0;
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] < nums[i - 1]) {
+                count++;
+                if(i < 2 || nums[i - 2] <= nums[i]) {
+                    nums[i - 1] = nums[i];
+                } else {
+                    nums[i] = nums[i - 1];
+                }
+            }
+            if(count >= 2) {
+                return false;
+            }
+        }
+        return count <= 1;
+    }
 }
