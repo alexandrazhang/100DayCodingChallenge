a11799f78b35b13a0ed54791acf6ccff2ccc0528
yanlizhang <alexandrazhang0626@gmail.com>
2018-05-14 13:46:56 -0700

    day9

---
+++ FromDay4/Day9LC412.java
@@ -0,0 +1,22 @@
+package CodingChallengeEasy.FromDay4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day9LC412 {
+    public List<String> fizzBuzz(int n) {
+        List<String> res = new ArrayList<>();
+        for(int i = 1; i <= n; i++) {
+            if(i % 3 == 0 && i % 5 == 0) {
+                res.add("FizzBuzz");
+            } else if(i % 3 == 0) {
+                res.add("Fizz");
+            } else if(i % 5 == 0) {
+                res.add("Buzz");
+            } else {
+               res.add(String.valueOf(i));
+            }
+        }
+        return res;
+    }
+}
